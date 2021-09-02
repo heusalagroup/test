5ddb556f88d6843a4ae13573cdb509020c66c166
Jaakko Heusala <jheusala@iki.fi>
2021-09-02 20:22:16 +0300

    Updated refs

---
+++ tools/update-git.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Update git and submodules from upstream
+
+(
+  set -e
+  set -x
+
+  git pull
+  (cd src/nor/pipeline/ && git pull)
+  (cd src/nor/ts/ && git pull)
+  (cd src/nor/ui/ && git pull)
+  (cd src/nor/matrix/ && git pull)
+
+) && echo Success || echo FAIL
