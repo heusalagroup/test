79b867443f6cf2f97e25681aaaf8cefc1348e6fb
Jaakko Heusala <jheusala@iki.fi>
2021-09-04 09:35:51 +0300

    Updated git scripts

--- tools/update-git.sh
+++ tools/update-git.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 # Update git and submodules from upstream
 
+if git status|grep -qF 'nothing to commit, working tree clean'; then
+  :
+else
+  git status
+  exit 1
+fi
+
 (
   set -e
   set -x
 
-  git pull
-  (cd src/nor/pipeline/ && git pull)
-  (cd src/nor/ts/ && git pull)
-  (cd src/nor/ui/ && git pull)
-  (cd src/nor/matrix/ && git pull)
+  git pull -q
+  if test -d src/nor/matrix/   ; then ( cd src/nor/matrix/   && git pull -q ); fi
+  if test -d src/nor/pipeline/ ; then ( cd src/nor/pipeline/ && git pull -q ); fi
+  if test -d src/nor/ts/       ; then ( cd src/nor/ts/       && git pull -q ); fi
+  if test -d src/nor/ui/       ; then ( cd src/nor/ui/       && git pull -q ); fi
 
 ) && echo Success || echo FAIL
