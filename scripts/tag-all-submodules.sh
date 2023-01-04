2632a5f855a5a2e72d65d810d52d9c9d50a675d6
Jaakko Heusala <jhh@heusalagroup.fi>
2023-01-05 00:50:34 +0200

    Fixed release workflow

--- scripts/tag-all-submodules.sh
+++ scripts/tag-all-submodules.sh
@@ -9,17 +9,11 @@ else
   exit 1
 fi
 set -e
-set -x
 
 cat .gitmodules |grep -F path|awk '{print $3}'|while read DIR; do
   (
     cd $DIR
     set -e
-#    if test "x$GITHUB_TOKEN" != x; then
-#      ORIGIN_URL="$(git remote get-url origin)"
-#      ORG_REPO="$(echo $ORIGIN_URL|sed -re 's@^.*github\.com[:/]@@')"
-#      git remote set-url origin "https://x-access-token:$GITHUB_TOKEN@github.com/$ORG_REPO"
-#    fi
     git tag -a "$TAG" -m "$MESSAGE"
     git push origin "$TAG"
   )
