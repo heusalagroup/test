f1639a9bf64f3e187c58660248bc39dd378cdf71
Jaakko Heusala <jhh@heusalagroup.fi>
2023-01-04 21:10:45 +0200

    Testing release with tagging support for all submodules

--- scripts/get-release-message.sh
+++ scripts/get-release-message.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 TAG="$1"
+cd "$(dirname "$0")/.."
 if test "x$TAG" = x; then
   TAG=$(git describe --tags --abbrev=0)
 fi
-cd "$(dirname "$0")/.."
 echo "This release contains the following changes since $TAG:"
 git log --oneline --no-merges --format="%s" "$TAG..HEAD"|sed -re 's/^/ * /g'
