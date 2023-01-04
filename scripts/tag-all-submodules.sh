e727bf31fc7c24fdefd68cfe3412528e2d2f52d8
Jaakko Heusala <jhh@heusalagroup.fi>
2023-01-04 23:22:40 +0200

    Testing release process

--- scripts/tag-all-submodules.sh
+++ scripts/tag-all-submodules.sh
@@ -9,6 +9,11 @@ else
   exit 1
 fi
 set -x
+
+if test "x$GITHUB_TOKEN" != x; then
+  git remote set-url origin "https://x-access-token:$GITHUB_TOKEN@github.com/$(git remote get-url origin|sed -re 's/git@github.com://' -e 's@https://github.com/@@')"
+fi
+
 cat .gitmodules |grep -F path|awk '{print $3}'|while read DIR; do
   (
     cd $DIR \
