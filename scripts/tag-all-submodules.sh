984207cd799952deff9fcf565baae3cefa466a59
Jaakko Heusala <jhh@heusalagroup.fi>
2023-01-05 00:45:40 +0200

    Testing release

--- scripts/tag-all-submodules.sh
+++ scripts/tag-all-submodules.sh
@@ -8,18 +8,18 @@ else
   echo 'USAGE: ./scripts/tag-all-submodules.sh TAG MESSAGE' >&2
   exit 1
 fi
+set -e
 set -x
 
 cat .gitmodules |grep -F path|awk '{print $3}'|while read DIR; do
   (
     cd $DIR
     set -e
-    set -x
-    if test "x$GITHUB_TOKEN" != x; then
-      ORIGIN_URL="$(git remote get-url origin)"
-      ORG_REPO="$(echo $ORIGIN_URL|sed -re 's@^.*github\.com[:/]@@')"
-      git remote set-url origin "https://x-access-token:$GITHUB_TOKEN@github.com/$ORG_REPO"
-    fi
+#    if test "x$GITHUB_TOKEN" != x; then
+#      ORIGIN_URL="$(git remote get-url origin)"
+#      ORG_REPO="$(echo $ORIGIN_URL|sed -re 's@^.*github\.com[:/]@@')"
+#      git remote set-url origin "https://x-access-token:$GITHUB_TOKEN@github.com/$ORG_REPO"
+#    fi
     git tag -a "$TAG" -m "$MESSAGE"
     git push origin "$TAG"
   )
