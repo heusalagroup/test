cb4d75a9fde2d5795f87502268dce4b6a0cc4e93
Jaakko Heusala <jhh@hg.fi>
2023-05-02 10:35:37 +0300

    Improved submodule listing

--- scripts/branch-all.sh
+++ scripts/branch-all.sh
@@ -2,21 +2,33 @@
 cd "$(dirname "$0")/.."
 #set -x
 
-echo -e "$(
+FORMAT='%-15s %-22s %-30s %-20s %s\n'
+
+ROOT_BRANCH="$(
     git branch \
      | grep -E '^\*' \
      | sed -re 's/^\* *//' \
      | tr -d '\n'
-    )\t."
+)"
+
+printf "$FORMAT" "$ROOT_BRANCH" "." "." "." "."
 
 cat .gitmodules |grep -F path|awk '{print $3}'|while read DIR; do  
   (
     cd $DIR
-    echo -e "$(
-    git branch \
-     | grep -E '^\*' \
-     | sed -re 's/^\* *//' \
-     | tr -d '\n'
-    )\t$DIR"
+    MODULE=$(echo $DIR|sed -re 's@^.*/src/@@'|tr '/' '.')
+    CONTEXT=$(echo $DIR|awk -F'/' '{print $1}')
+    TAG=$((git describe --tags 2>/dev/null)|sort -n|tail -n1)
+    if test "x$TAG" = x; then
+      TAG="$(git rev-parse --short HEAD)"
+    fi
+    BRANCH="$(
+      git branch \
+       | grep -E '^\*' \
+       | sed -re 's/^\* *//' \
+       | tr -d '\n'
+    )"
+
+    printf "$FORMAT" "$BRANCH" "$CONTEXT" "$MODULE" "$TAG" "$DIR"
   ); 
 done
