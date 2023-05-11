ebe5f743e2d3f9d6bca4ae6a3e1711e256e27cfd
Jaakko Heusala <jhh@hg.fi>
2023-05-11 10:47:17 +0300

    Refactored fi.hg.repository as part of fi.hg.core, fi.hg.mysql and fi.hg.pg, deprecating fi.hg.repository

--- scripts/reset-integration-test-pg.sh
+++ scripts/reset-integration-test-pg.sh
@@ -2,4 +2,4 @@
 cd "$(dirname "$0")/.."
 set -x
 
-docker run -i --rm postgres:15 psql 'postgresql://hg:rxPe4XHbonLeHwjvKdq9R9aDWf1xiUje@host.docker.internal/hg' < ./src/fi/hg/repository/integration/pg/initial.sql
+docker run -i --rm postgres:15 psql 'postgresql://hg:rxPe4XHbonLeHwjvKdq9R9aDWf1xiUje@host.docker.internal/hg' < ./src/fi/hg/pg/docker/initial.sql
