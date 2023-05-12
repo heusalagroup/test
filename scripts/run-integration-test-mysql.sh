07f8fe3c2119e2e1fa3b28666ae08f31edb99265
Jaakko Heusala <jhh@hg.fi>
2023-05-12 23:43:26 +0300

    Working on better data persisters

--- scripts/run-integration-test-mysql.sh
+++ scripts/run-integration-test-mysql.sh
@@ -2,4 +2,5 @@
 cd "$(dirname "$0")/.."
 set -x
 
-TEST_MYSQL_PASSWORD='rxPe4XHbonLeHwjvKdq9R9aDWf1xiUje' TEST_SCOPES=integration,hg npm test
+npm run clear
+TEST_MYSQL_PASSWORD='rxPe4XHbonLeHwjvKdq9R9aDWf1xiUje' TEST_SCOPES=integration,hg npm run test:ci -- MySqlRepositoryIntegration.test.ts "$@"
--- scripts/run-integration-test-pg.sh
+++ scripts/run-integration-test-pg.sh
@@ -2,4 +2,5 @@
 cd "$(dirname "$0")/.."
 set -x
 
-TEST_POSTGRES_PASSWORD='rxPe4XHbonLeHwjvKdq9R9aDWf1xiUje' TEST_SCOPES=integration,hg npm test
+npm run clear
+TEST_POSTGRES_PASSWORD='rxPe4XHbonLeHwjvKdq9R9aDWf1xiUje' TEST_SCOPES=integration,hg npm run test:ci -- 'PgRepositoryIntegration.test.ts' "$@"
