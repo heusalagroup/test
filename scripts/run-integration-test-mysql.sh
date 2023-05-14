facf60ed426eee70dc1df6289ca10c55e0f42702
Jaakko Heusala <jhh@hg.fi>
2023-05-15 00:51:30 +0300

    Small fix for integration test scripts

--- scripts/run-integration-test-mysql.sh
+++ scripts/run-integration-test-mysql.sh
@@ -3,4 +3,4 @@ cd "$(dirname "$0")/.."
 set -x
 
 npm run clear
-TEST_MYSQL_PASSWORD='rxPe4XHbonLeHwjvKdq9R9aDWf1xiUje' TEST_SCOPES=integration,hg npm run test:ci -- MySqlRepositoryIntegration.test.ts "$@"
+TEST_MYSQL_PASSWORD='rxPe4XHbonLeHwjvKdq9R9aDWf1xiUje' TEST_SCOPES=integration,hg npm run test:ci -- "$@" MySqlRepositoryIntegration.test.ts
